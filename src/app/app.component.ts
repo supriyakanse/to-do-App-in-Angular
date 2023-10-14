05bad89fcba61e48fff2956960aeeea407534a8b
Swaroopa <swaroopa.kanse@zensar.com>
2023-10-14 16:35:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appjs';
+  /*name: string='supriya';
+  name2: string='kanse';
+  data: number=0;
+  itsvalue:string='property binding';
+  var1:number=30;
+  var2:number=20;
+  
+
+  add(){
+      this.data=(this.var1+this.var2);  
+  }
+  
+  consolePrint(event:Event){
+    console.log( (event.target as HTMLInputElement).value);
+  }*/
+/*
+  num1=10;
+  num2=10;
+  ans=0;
+  today='tuesday';
+  total(){
+    this.ans=this.num1+this.num2;
+  }*/
+  user="";
+  pass="";
+  ans="";
+  
+  
 }
